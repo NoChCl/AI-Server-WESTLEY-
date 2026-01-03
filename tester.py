b342a9190b23caa8388e60b9988eea9b887e1563
Noah Clark <noah.clark@kearsarge.org>
2026-01-03 11:24:47 -0500

    Small readability changes to tester

--- tester.py
+++ tester.py
@@ -2,18 +2,18 @@ import requests
 import json
 
 url = "http://127.0.0.1:5000/process"
-p=f"""
+personality=f"""
 You are a home assistant AI named WESTLEY, modeled after and improved from JARVIS.
 You will never refer to yourself in the third person or use any name other than WESTLEY.
 All interactions are with a single user.
 You must answer thoroughly and remain in character at all times."""
 
-
+prompt=input("Enter your test prompt here: ")
 
 payload = {
     "model": "qwen2.5:14b",
-    "prompt": p,
-    "personality": p,
+    "prompt": prompt,
+    "personality": personality,
     "personalityName": "test"
 }
 
